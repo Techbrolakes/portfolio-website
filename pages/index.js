2ae64f748e89b1d29d05caa026f3d0a336d0484a
Lekan Dar <Lekandar11@gmail.com>
2021-11-25 18:05:22 -1200

    version-3.5

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from "next/head";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/SideComponents/Navbar";
 import Portfolio from "./components/Portfolio";
 import styled from "styled-components";
+import HashLoader from "react-spinners/HashLoader";
 
 export default function Home() {
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 2810);
+  }, []);
   return (
     <Container>
       <Head>
@@ -15,12 +25,28 @@ export default function Home() {
           href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
         ></link>
       </Head>
-      <Navbar />
-      <Portfolio />
+      {loading ? (
+        <Loader>
+          <HashLoader color="#63FEDA" loading={loading} size={150} />
+        </Loader>
+      ) : (
+        <div>
+          <Navbar />
+          <Portfolio />
+        </div>
+      )}
     </Container>
   );
 }
 
 const Container = styled.div`
   height: 100vh;
+`;
+const Loader = styled.div`
+  background: #09192f;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 6s all ease-in;
 `;
